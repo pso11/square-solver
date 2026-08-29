6f31c747a2f1ed8df8da6f2b276d4d3a5524f93c
pso11 <sofiya.pshenichnaya.09@mail.ru>
2026-08-29 17:55:27 +0300

    +документация

--- choose_color.c
+++ choose_color.c
@@ -1,52 +1,64 @@
+/**
+ * @file choose_color.c
+ * @brief File containing the program's text color selection functionю
+ * @author Pshanichnaya Sofia
+ * @date 2026-08-28
+ */
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <TXLib.h>
 
-#define ALPHA 'H'
-
+#include "input_and_output.h"
 #include "choose_color.h"
 
 void choose_color(void)
 {   
-    printf(DARK_BLUE"Enter color name from table to change text color of your programm\n");
-    triangle(ALPHA);
+    delayed_output("Enter color name from table to change text color of your programm\n" 
+                    RESET, TIME_DELAY);
     printf("\n");
-    printf(RED"    1. Black        5. Dark Blue\n"
-              "    2. Red          6. Pink\n"
-              "    3. Green        7. Light Blue\n"
-              "    4. Yellow       8. White\n"RESET);
+    printf("    1. %sBlack        " RESET"5. %sDark Blue\n" RESET               
+           "    2. %sRed          " RESET"6. %sPink\n" RESET
+           "    3. %sGreen        " RESET"7. %sLight Blue\n" RESET
+           "    4. %sYellow       " RESET"8. %sWhite\n" RESET, BLACK, DARK_BLUE, 
+                RED, PINK, GREEN, LIGHT_BLUE, YELLOW, WHITE);
+
     struct color_table array_color[] =
     {
-        {"black", BLACK},
-        {"red", RED},
-        {"yellow", YELLOW},
-        {"dark_blue", DARK_BLUE},
-        {"pink", PINK},
-        {"light_blue", BLUE},
-        {"white", WHITE}
+        {"black",      BLACK},
+        {"red",        RED},
+        {"yellow",     YELLOW},
+        {"dark blue",  DARK_BLUE},
+        {"pink",       PINK},
+        {"light blue", LIGHT_BLUE},
+        {"white",      WHITE},
+        {"green",      GREEN}
     };
-    int amount_of_colors = sizeof(array_color) / sizeof(array_color[0]);
+
+    int amount_of_colors = sizeof(array_color) / sizeof(array_color[0]);   
     const char* selected_color = RESET;
-    char color_input[MAX_LENGTH_COLOR_NAME] = "";
-    char* p = color_input;
-    int c = 0;
+
+    char input_color[MAX_LENGTH_COLOR_NAME] = "";                          
+    char* p = input_color;
+    int c = 0, length_color_name_input = 0;
     while ((c = getchar()) != '\n' && c != EOF)
     {
         c = tolower(c);
-        *(p++) = c;
+        *(p++) = (char)c;
+        length_color_name_input++;
     }
     *p = '\0';
-    int i = 0;
-    for (int i = 0; i < amount_of_colors; i++)
+    
+    for (int i = 0; i < amount_of_colors; i++)                            
     {
-        if (!strcmp(color_input, array_color[i].name))
+        if (!strcmp(array_color[i].name, input_color))
         {
-            selected_color =  array_color[i].code;
+            selected_color = array_color[i].code;
             break;
         }
+        
     }
     if (selected_color == RESET)
-           printf(RED"Please OPEN your eyes and READ the table again!\n"
-                  "Try next time\n"RESET);
+        delayed_output("Please OPEN your eyes and READ the table again to try your best next time", TIME_DELAY);
     printf("%s", selected_color);
 }
