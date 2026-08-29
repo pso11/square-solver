9f52806e2b51bed8f411ee8afee01c625bef77cc
pso11 <sofiya.pshenichnaya.09@mail.ru>
2026-08-29 18:17:24 +0300

    Документация

--- solving.h
+++ solving.h
@@ -1,12 +1,46 @@
-/* Минимальная разница между двумя переменными типа double для их равенства */
+/**
+ * @file solving.h
+ * @brief  Header file for solving.c.
+ * @author Pshanichnaya Sofia
+ * @date 2026-08-28
+ */
+
+/**
+ * @brief Constant for comparing two numbers type double.
+ * @details When we compare two doubles and difference between them less then DELTA they're equal.
+ */ 
 #define DELTA 10e-6
 
-/* Сравнение двух чисел типа double */
-int compare_doubles(double, double);
+/**
+ * @brief  Compares two double-precision numbers for equality.
+ * @param[in] var1 A single variable of type double.
+ * @param[in] var2 The second variable of type double.
+ * @return return either true or false.
+ */
+int compare_doubles(double var1, double var2);
 
-/* Решение квадратного уравнения вида y = ax^2 + bx + c */
-int solving_quadratic(double, double, double, double*, double*);
+/**
+ * @brief  Solves a quadratic equation.
+ * @param[in] a The coefficient before x^2.
+ * @param[in] b The coefficient before x.
+ * @param[in] c The constant term.
+ * @param[out] x1 Pointer to the first root.
+ * @param[out] x2 Pointer to the second root.
+ * @return return amount of real roots.
+ * @see compare_doubles()
+ */
+int solving_quadratic(double a, double b, double c, double* x1, double* x2);
 
-/* Решение линейного уравнения вида y = ax + b */
+/**
+ * @brief  Solves a linear equation type y(x) = a*x + b.
+ * @param[in] a The coefficient before x.
+ * @param[in] b The constant term.
+ * @param[out] x1 Pointer to the root.
+ * @return amount of real roots
+ */
 int solving_linear(double, double, double*);
 
+/**
+ * @brief  Clears the buffer.
+ */
+void buffer_cleaning(void);
